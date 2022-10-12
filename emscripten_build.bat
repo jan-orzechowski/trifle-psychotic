3a3a436e60576fda59af770e1e39fb31f370d96e
jor <orzechowski.jan@gmail.com>
2022-10-12 21:35:14 +0200

    add batch script for Emscripten build

---
+++ emscripten_build.bat
@@ -0,0 +1,13 @@
+cd /d C:\Emscripten\emsdk-3.1.23 
+call emsdk_env.bat
+
+cd /d D:\JO_Programowanie\TriflePlatformer
+
+call emcc -std=c++14 -fno-rtti -fno-exceptions -Wno-switch code/sdl_platform.cpp code/animation.cpp code/collision.cpp code/debug.cpp code/entities.cpp code/game_data.cpp code/gates.cpp code/input.cpp code/jormath.cpp code/jorstring.cpp  code/level_parsing.cpp code/main.cpp code/map.cpp code/player.cpp code/rendering.cpp code/text_rendering.cpp code/tmx_parsing.cpp code/ui.cpp -o build/wasm/trifle_psychotic.html --preload-file data --preload-file gfx -sUSE_SDL=2 -sUSE_SDL_IMAGE=2 -sSDL2_IMAGE_FORMATS='""png""' -sALLOW_MEMORY_GROWTH=1 -sINITIAL_MEMORY=128MB -sTOTAL_STACK=32MB -g3 -gsource-map --source-map-base http://localhost:6931/ --emrun --memoryprofiler
+
+REM wersja niedebugowa:
+REM call emcc -std=c++14 -fno-rtti -fno-exceptions -v code/animation.cpp code/collision.cpp code/debug.cpp code/entities.cpp code/game_data.cpp code/gates.cpp code/input.cpp code/jormath.cpp code/jorstring.cpp  code/level_parsing.cpp code/main.cpp code/map.cpp code/player.cpp code/rendering.cpp code/sdl_platform.cpp code/text_rendering.cpp code/tmx_parsing.cpp code/ui.cpp -o build/wasm/trifle_psychotic.html --preload-file data --preload-file gfx -sUSE_SDL=2 -sUSE_SDL_IMAGE=2 -sSDL2_IMAGE_FORMATS='""png""' -sSAFE_HEAP=1 -sALLOW_MEMORY_GROWTH=1 -sINITIAL_MEMORY=128MB -sTOTAL_STACK=32MB -o3
+
+pause
+
+call emrun --browser "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe" build/wasm/trifle_psychotic.html
